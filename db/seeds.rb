b6b84ea458b475142d166a8fa9c9988315ec0b02
Zainab-Alzubair <zainab.a.elzubaie@gmail.com>
2023-06-12 14:05:32 +0300

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,44 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+admin = User.create!(
+  email: 'admin@example.com',
+  password: 'password',
+  name: 'Admin',
+  surname: 'User',
+  role: 'admin'
+)
+
+user = User.create!(
+  email: 'user@example.com',
+  password: 'password',
+  name: 'Regular',
+  surname: 'User',
+  role: 'user'
+)
+
+Slot.create!(
+  time: 'Morning',
+  car_type: 'Hybrid / Electric',
+  price: 500,
+  is_disabled: false,
+  is_available: true,
+  cancelation_policy: '24 hours',
+  availability_start_time: '',
+  availability_end_time: ''
+)
+
+Slot.create!(
+  time: 'Evening',
+  car_type: 'Truck',
+  price: 700,
+  is_disabled: true,
+  is_available: false,
+  cancelation_policy: '48 hours',
+  availability_start_time: Time.zone.parse('14:00'),
+  availability_end_time: Time.zone.parse('18:00')
+)
+
 working_hours = [
   { day: 'Sunday', start_time: '09:00', end_time: '17:00' },
   { day: 'Monday', start_time: '08:30', end_time: '16:30' },
